247c16be52ce8cb2cf159b35dbc33de0115eb7c9
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2025-04-07 18:14:05 +0500

    Completed square.py task

---
+++ Assignment-00-05/00_intro_python/square.py
@@ -0,0 +1,11 @@
+import math
+def main():
+    user_input : float = float(input("Type a number to see it's square :"))
+    user_input : float = float(user_input)
+    result : float = float(user_input **2)
+    print(f"{user_input} squared is : ", result)
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
