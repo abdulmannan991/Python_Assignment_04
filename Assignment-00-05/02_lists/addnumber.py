5ebb0bdb646bf4ff57d751536fdaa012a74e8890
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2025-04-13 13:16:38 +0500

    Completed double-list

--- Assignment-00-05/02_lists/addnumber.py
+++ Assignment-00-05/02_lists/addnumber.py
@@ -1,7 +1,9 @@
-def addNumbers(lst):
+def addNumbers():
+    lst = [1,2,3,4,5,6]
     sum = 0
     for i in lst:
         sum += i
     print(f"The sum of numbers {lst} is: {sum}")
     
-addNumbers([1,2,3,4,5,6])
+addNumbers()
+
---
+++ Assignment-00-05/02_lists/double-list.py
@@ -0,0 +1,10 @@
+def main():
+    lst = [1,2,3,4]
+    new_lst = [i * 2 for i in lst]
+    print(f"The old list {lst} The new list: {new_lst}")
+
+
+
+
+if __name__ == '__main__':
+    main()
